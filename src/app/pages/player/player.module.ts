e97d278fc76365b7446f0d191249f264f90e68e6
Anderson Menezes <anderson.m.menezes@accenture.com>
2022-11-27 18:10:49 -0300

    create and config module Player

---
+++ src/app/pages/player/player.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayerComponent } from './player.component';
+import { RouterModule } from '@angular/router';
+import { PlayerRoutes } from './player.routes';
+
+@NgModule({
+  declarations: [
+    PlayerComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(PlayerRoutes)
+  ]
+})
+export class PlayerModule { }
